e5c92b67972532f15af526ecef060226a8f1c2e7
Jacob Danner <jacob.danner@gmail.com>
2022-09-02 16:05:00 -0700

    Initial Commit of new structure and trying to use znai as content source, need to check github workflow

---
+++ spring-app-examples/src/main/java/com/ph477y/PagesSiteApplication.java
@@ -0,0 +1,13 @@
+package com.ph477y;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagesSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagesSiteApplication.class, args);
+	}
+
+}
---
+++ spring-app-examples/src/test/java/com/ph477y/PagesSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ph477y;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagesSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
